9c33a2653c8f13ecb18bcb7f034eac299dee854b
David Frost <davidmichaelfrost@gmail.com>
2019-01-09 17:14:08 -0500

    Setup user up and down migration and add mysql module

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -5,17 +5,31 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.timestamps()
-    })
+    this.raw(`
+      CREATE TABLE users(
+          PRIMARY KEY (id),
+          id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+          first_name VARCHAR(60)  NOT NULL,
+          last_name  VARCHAR(60)  NOT NULL,
+          email      VARCHAR(60)  NOT NULL UNIQUE,
+          birth_date DATE         NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )
+      `)
+    // this.create('users', (table) => {
+    //   table.increments()
+    //   table.string('username', 80).notNullable().unique()
+    //   table.string('email', 254).notNullable().unique()
+    //   table.string('password', 60).notNullable()
+    //   table.timestamps()
+    // })
   }
 
   down () {
-    this.drop('users')
+    this.raw(`
+      DROP TABLE users
+      `)
   }
 }
 
